e791271aa784f9726aac49d1404e61787c352d79
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-22 23:07:58 -0500

    Final Square edits

--- src/com/company/Square.java
+++ src/com/company/Square.java
@@ -23,11 +23,17 @@ public class Square {
     }
 
     public void squareInfo(){
-        System.out.print("Each side of the square is " + getSideLength() + " inches long");
+
+        System.out.print("SQUARE PROPERTIES");
         System.out.print("\n");
-        System.out.print ("The square's area is " + calculateSquareArea() + " square inches");
+        System.out.print("--------------------------------------------");
         System.out.print("\n");
-        System.out.print ("Fun Fact: A Square is also a rectangle with equal sides" );
+        System.out.print("--Each side of the square is " + getSideLength() + " inches long");
+        System.out.print("\n");
+        System.out.print ("--The square's area is " + calculateSquareArea() + " square inches");
+        System.out.print("\n");
+        System.out.print ("--Fun Fact: A Square is also a rectangle with equal sides" );
+        System.out.print("--------------------------------------------");
 
 
     }
